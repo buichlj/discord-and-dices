2b1886c4eab4afd1f07e04dba906ee0b6c7f822d
buichlj <jbuichl@gmail.com>
2022-01-08 15:18:08 -0600

    remove debug statements

--- app/message_handler.py
+++ app/message_handler.py
@@ -12,7 +12,6 @@ class MessageHandler:
             if message_lower.startswith('$hello'):
                 return_string = f'Hello {message.author}!'
             elif message_lower.startswith('$roll') or message_lower.startswith('$r'):
-                print("HERE1")
                 return_string = self.roll_dice(message_lower)
             elif message_lower.startswith('$help'):
                 return_string = self.help_message()
@@ -23,25 +22,18 @@ class MessageHandler:
         return return_string
 
     def roll_dice(self, dice_string):
-        print("HERE2")
-        print(dice_string)
         number = None
         die = None
         adder = None
         multiplyer = False
         retValue = 0
-        print("HERE5")
         dice_string = dice_string.replace(
             ' ', '').replace('$roll', '').replace('$r', '')
         dice_array = dice_string.split('d')
-        print("HERE6")
 
         if(len(dice_array) != 2):
-            print("HERE3")
             raise Exception("Invalid command")
-        print("HERE8")
         number = int(dice_array[0])
-        print("HERE9")
         if('(+' in dice_array[1]):
             multiplyer = True
             dice_array[1] = dice_array[1].replace('(', '').replace(')', '')
@@ -50,14 +42,11 @@ class MessageHandler:
             temp_array = dice_array[1].split('+')
 
             if(len(temp_array) != 2):
-                print("HERE4")
                 raise Exception("Invalid command")
 
             dice_array[1] = temp_array[0]
             adder = temp_array[1]
-        print("HERE7")
         die = int(dice_array[1])
-        print("HERE10")
         for x in range(number):
             retValue += random.randrange(1, die+1)
 
